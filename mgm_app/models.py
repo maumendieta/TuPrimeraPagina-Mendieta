3a3e2f5ef3a4f46474a6a1c05bc4077004e9da03
Mauro <maurogmen@gmail.com>
2025-06-28 11:39:00 -0300

    Creación de primer modelo

--- mgm_app/models.py
+++ mgm_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+    
+    def save(self):
+        print("Guardando usuario...")
+        super().save()
